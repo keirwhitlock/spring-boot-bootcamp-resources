3ee6f1c52e6930e378c9ffa395cdab720d47c52b
Keir Whitlock <11267934+keirwhitlock@users.noreply.github.com>
2025-07-08 18:44:26 +0100

    Update Grade.java

--- 06. testing/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/pojo/Grade.java
+++ 06. testing/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/pojo/Grade.java
@@ -21,6 +21,12 @@ public class Grade {
         this.id = UUID.randomUUID().toString();
     }
 
+    public Grade(String name, String subject, String score) {
+        this.name = name;
+        this.subject = subject;
+        this.score = score;
+    }
+
     public String getName() {
         return name;
     }
